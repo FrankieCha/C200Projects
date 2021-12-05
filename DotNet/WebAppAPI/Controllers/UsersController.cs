84ec6a995678371c9bcd24580390b8794e3233b2
FrankieCha <frankie_cha@rp.edu.sg>
2021-12-05 17:15:45 +0800

    Renamed WebAPI project to WebAppAPI

---
+++ DotNet/WebAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using C200.WebApi.Models;
+using C200.WebApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+// http://jasonwatmore.com/post/2019/01/08/aspnet-core-22-role-based-authorization-tutorial-with-example-api#users-controller-cs
+// https://www.c-sharpcorner.com/article/authentication-authorization-using-net-core-web-api-using-jwt-token-and/
+
+namespace C200.WebApi.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+   private readonly IAccountService _accSvc;
+   private readonly IDBService _sbSvc;
+
+   public UsersController(IAccountService accountService,
+                           IDBService dbService)
+   {
+      _accSvc = accountService;
+      _sbSvc = dbService;
+   }
+
+   [AllowAnonymous]
+   [HttpPost("Authenticate")]
+   public IActionResult Authenticate([FromBody]Login credential)
+   {
+      var user = _accSvc.Authenticate(credential);
+
+      if (user == null)
+         return BadRequest(new { message = "Username or password is incorrect" });
+         
+      return Ok(user.Token);
+   }
+
+   [Authorize(Roles = "manager")]
+   [HttpGet("Roles")]
+   public ActionResult<IEnumerable<string>> Get()
+   {
+      DataTable dt = _sbSvc.GetTable("SELECT DISTINCT UserRole FROM SysUser");
+      List<String> result = dt
+         .AsEnumerable()
+         .Select(row => new String((String)row["UserRole"]))
+         .ToList();
+
+      return result;
+   }
+
+   [Authorize(Roles = "admin")]
+   [HttpGet("Users")]
+   public IActionResult GetAll()
+   {
+      var users = 
+         _accSvc
+            .GetAll()
+            .Select(
+               user => new {user.UserId, user.FullName, user.Email, user.UserRole})
+            .ToList();
+        
+      return Ok(users);
+   }
+
+   [Authorize]
+   [HttpGet("{id}")]
+   public IActionResult GetById(string id)
+   {
+      var user = _accSvc.GetById(id);
+      if (user == null)
+      {
+         return NotFound();
+      }
+         
+      return Ok(new {user.UserId, user.FullName, user.Email, user.UserRole});
+   }
+}
+
---
+++ DotNet/WebAppAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace C200.WebApi.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)] // Suppress Swagger
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+   // GET api/values/5
+   [HttpGet("{id}")]
+   public ActionResult<string> Get(int id)
+   {
+      return "value";
+   }
+
+}
